1afe8fc87f751505943506bfb9407e5a2f04e2f6
Riyam111 <riyam1680@gmail.com>
2025-06-08 23:21:16 +0530

    change my mongo db

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,11 +1,10 @@
 import mongoose from "mongoose";
-
-
-
-
 const connectDB = async () => {
-  mongoose.connection.on('connected',()=>console.log('database connected'))
-  await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
+  mongoose.connection.on('connected', () => console.log('database connected'));
+  await mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 };
 
 export default connectDB;
